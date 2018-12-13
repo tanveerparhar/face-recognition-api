d391753c9d79f1f8cb2552fd3e72a34322eebef3
Tanveer Singh <31854574+tanveerparhar@users.noreply.github.com>
2018-12-14 03:39:38 +0530

    uploading server files

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require ('knex');
+const bcrypt = require ('bcrypt-nodejs');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+////////////connnecting database
+const db=knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '100parhar',
+    database : 'facerecognition_db'
+  }
+});
+
+ //db.select('*').from('users');
+const app = express(); //server starts
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/',(req,res) =>{
+res.json(database.users);
+});
+
+app.post('/signin',(req,res) =>{signin.handleSignin(req, res, db, bcrypt) });
+///different method used new syntax
+app.post('/register', register.handleRegister(db, bcrypt) );
+
+app.get('/profile/:id',(req,res) =>{profile.handleProfileGet(req, res, db) });
+
+app.put('/image',(req,res) =>{image.handleImage(req, res, db )});
+
+app.post('/imageurl', (req,res) =>{image.handleApiCall(req, res) });
+
+app.listen(3000,()=>{
+	console.log('app is listening to port 3000');
+});
